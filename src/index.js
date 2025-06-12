3d29fec5afcea0b1652e5aa82c9c9d6241486174
Tal KRIEF <talkrief@gmail.com>
2025-06-12 17:12:51 +0200

    removed splash cursor sorry mom

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import './style.css';
 import './congratulations.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import SplashCursor from './SplashCursor'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <SplashCursor></SplashCursor>
   </React.StrictMode>
 );
 
